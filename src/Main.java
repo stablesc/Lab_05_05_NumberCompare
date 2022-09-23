d4a4f071bb82a08db23fec01f2aa75009743d812
Steele <stablesc@mail.uc.edu>
2022-09-22 20:54:22 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double numberA = 25.5;
+        double numberB = 25.6;
+
+        if (numberA == numberB)
+        {
+            System.out.println("These numbers are equal");
+        }
+        else if (numberA > numberB)
+        {
+            System.out.println("Number A is greater than Number B ");
+        }
+        else //Number B must be Greater than Number A
+        {
+            System.out.println("Number A is less than Number B");
+        }
     }
 }
